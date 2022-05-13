1b432ed1ec98220cafba51c5d6efabab2a09052d
DavideBernardini <davidebernardini09@gmail.com>
2022-05-13 10:31:18 +0200

    refactoring

--- src/components/navigatore.jsx
+++ src/components/navigatore.jsx
@@ -4,11 +4,11 @@ import { Globe } from "react-bootstrap-icons";
 
 const Navigatore = () => {
     return (
-        <Navbar bg="none" expand="lg">
+        <Navbar bg="none" expand="lg" className="flex-nowrap">
             <Container fluid>
-                <Navbar.Brand><img className="ms-5" style={{ width: "200px", height: "auto" }} src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+                <Navbar.Brand><img className="ms-3 ms-md-5" style={{ width: "200px", height: "auto" }} src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" /></Navbar.Brand>
+                
+                <div id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
                         style={{ maxHeight: '100px' }}
@@ -17,13 +17,13 @@ const Navigatore = () => {
 
                     </Nav>
                     <Form className="d-flex m-4">
-                        <Form.Select  aria-label="Default select example">
+                        <Form.Select  aria-label="Default select example" style={{ minWidth: '110px' }}>
                             <option>Italiano</option>
                             <option value="1"><Globe/>English</option>
                         </Form.Select>
-                        <Button id="btn-access" className="mx-4 px-3" variant="danger">Accedi</Button>
+                        <Button id="btn-access" className="ms-2 mx-md-4 px-3" variant="danger">Accedi</Button>
                     </Form>
-                </Navbar.Collapse>
+                </div>
             </Container>
         </Navbar>
     )
