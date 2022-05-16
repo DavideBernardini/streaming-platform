84cafab9054797544e45db449d616bcfba819d1f
DavideBernardini <davidebernardini09@gmail.com>
2022-05-16 12:45:28 +0200

    fetch in progress

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MoviesRow from "./MoviesRow";
+
+const discoverMovieUrl = "localhost:2000/api/movie/search/";
+
+const idHorror = 27;
+
 
 const MainPage = () => {
+    let [horrorMovies, setHorrorMovies] = useState(null);
+
+    useEffect(() => {
+        fetchHorror()
+    }, [])
+    
+    const fetchHorror = () => {
+        axios.get(discoverMovieUrl + idHorror)
+        .then((response) => {
+            setHorrorMovies(response.data)
+            console.log(response.data);
+            console.log(horrorMovies);
+        })
+    }
+
     return(
         <>
-        
+        <MoviesRow category="Horror"/>
+
         </>
     )
 }
