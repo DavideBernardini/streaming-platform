fc23730f2c692d6d1ce800171c5d85b9f59420a6
DavideBernardini <davidebernardini09@gmail.com>
2022-05-16 10:35:29 +0200

    fix react slick

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 /* bootstrap */
 import 'bootstrap/dist/css/bootstrap.min.css';
 // react-slick
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
