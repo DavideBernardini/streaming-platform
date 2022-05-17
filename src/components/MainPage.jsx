5d03f4bdbd3b4502c6f66c843cbb96a30e68a7ae
DavideBernardini <davidebernardini09@gmail.com>
2022-05-17 17:40:48 +0200

    refactoring

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -100,10 +100,7 @@ const MainPage = () => {
     };
 
     return (
-        <div>
-            <h1>1</h1>
-            <h1>2</h1>
-            <h1>3</h1>
+        <div className="main-page">
             <CercaFilm />
             {/* movie rows */}
             <MoviesTvRow category="Azione:" moviesOrTv={actionMovies} />
