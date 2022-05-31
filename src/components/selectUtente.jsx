0cf0e651777a56ff42b9deb250f5ff66eb8305e1
DavideBernardini <davidebernardini09@gmail.com>
2022-05-31 08:59:22 +0200

    fix style hero

--- src/components/selectUtente.jsx
+++ src/components/selectUtente.jsx
@@ -8,23 +8,23 @@ const SelectUtente = () => {
             <div className="contenitoreGrande text-center" >
                 
                 <div className="profili">
-                    <h1>Chi vuole Guardare GreenStream?</h1>
+                    <h1 className="text-white fw-bold mb-4">Chi vuole Guardare GreenStream?</h1>
                         <ul className='profileList flex-wrap'>
                             <li >
                                 <Link to="/mainPage"><img src={require("../images/avatar1.png")} alt="profile thumb"></img></Link>
-                                <div className='fs-4 fw-bold'>Alessandro</div>
+                                <div className='fs-4 mt-2 text-white fw-bold'>Alessandro</div>
                             </li>
                             <li>
                             <Link to="/mainPage"><img src={require("../images/avatar2.png")} alt="profile thumb"></img></Link>
-                            <div className='fs-4 fw-bold'>Davide</div>
+                            <div className='fs-4 mt-2 text-white fw-bold'>Davide</div>
                             </li>
                             <li>
                             <Link to="/mainPage"><img src={require("../images/avatar3.png")} alt="profile thumb"></img></Link>
-                            <div className='fs-4 fw-bold'>Valerio</div>
+                            <div className='fs-4 mt-2 text-white fw-bold'>Valerio</div>
                             </li>
                             <li>
                             <Link to="/mainPage"><img src={require("../images/avatar4.png")} alt="profile thumb"></img></Link>
-                            <div className='fs-4 fw-bold'>Loris</div>
+                            <div className='fs-4 mt-2 text-white fw-bold'>Loris</div>
                             </li>
                         </ul>
             </div>
